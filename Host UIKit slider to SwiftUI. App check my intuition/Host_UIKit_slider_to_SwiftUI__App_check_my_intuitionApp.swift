486fda622333c7ecef12e2d6381bf7d8cc6295b0
Oxyenniu <oxyenniu@gmail.com>
2023-01-22 14:31:24 +0300

    Initial Commit

---
+++ Host UIKit slider to SwiftUI. App check my intuition/Host_UIKit_slider_to_SwiftUI__App_check_my_intuitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Host_UIKit_slider_to_SwiftUI__App_check_my_intuitionApp.swift
+//  Host UIKit slider to SwiftUI. App check my intuition
+//
+//  Created by Oxyenniu on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Host_UIKit_slider_to_SwiftUI__App_check_my_intuitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
